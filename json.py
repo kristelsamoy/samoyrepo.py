794b23af34ef30be324c90f377fa7c6994c74792
samoypy <75332621+kristelsamoy@users.noreply.github.com>
2021-03-02 16:03:56 +0100

    libreria

---
+++ json.py
@@ -0,0 +1,16 @@
+import json
+
+dati = {
+     "uno" : [marco],
+     "due" : {"uno" : "nome" , "due" : "cognome" },
+     "tre" : [ (marco ,), (30 anni , scrittore ), ( felice , avventura , italiano), ( biografico , fantascienza , giallo)],
+     "quattro": [ 30 anni ]
+     "cinque" : [biografico]
+     "sei" : [avventura]
+     "sette" : [fantascienza]
+     "otto" : [italiano]
+     "nove" : [giallo]
+     "dieci" : { "uno" : marco , "due" : felice , "tre" : maschio , "quattro": 30 anni  , "cinque" : biografico , "sei" : napoli , "sette" : fantascienza , "otto" : italiano , "nove" : giallo }
+
+
+}
